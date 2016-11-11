49e8bc3fe81bb07b2a3813dae9aeb2ee883190c2
khacviet-techmaster <khacviet@techmasters-mac-mini.local>
2016-11-11 15:41:31 +0700

    Initial Commit

---
+++ Login-TableTests/Login_TableTests.swift
@@ -0,0 +1,36 @@
+//
+//  Login_TableTests.swift
+//  Login-TableTests
+//
+//  Created by khacviet on 11/11/16.
+//  Copyright © 2016 khacviet. All rights reserved.
+//
+
+import XCTest
+@testable import Login_Table
+
+class Login_TableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
